8d14860a6ad4c45824e02d03f31b5dbceb83a6fd
nulla2011 <nulla@outlook.com>
2022-05-10 15:04:18 +0800

    release

--- userscript/anisoninfo-enhance.user.js
+++ userscript/anisoninfo-enhance.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         anisoninfo enhance
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.3
 // @description  replace real links in anison.info
 // @author       nulla
 // @match        http://anison.info/*
@@ -14,25 +14,20 @@
   function changeLinks(table) {
     let a = table.querySelectorAll('a');
     for (let el of a) {
-      let match = el.getAttribute('href').match(/javascript:link\('(\w+)','(\d+)'\)/);
+      let match = el.href.match(/javascript:link\('(\w+)','(\d+)'\)/);
       if (!match) {
         console.error('not found!');
       } else {
         let [catgory, id] = match.slice(1);
-        el.setAttribute('href', `${location.origin}/data/${catgory}/${id}.html`);
+        el.href = `/data/${catgory}/${id}.html`;
       }
     }
   }
   let mainTable = document.querySelectorAll('table.sorted');
-  for (let t of mainTable) {
-    if (t.tHead) {
-      changeLinks(t);
-    }
-  }
   let infoTable = document.querySelectorAll('table.list');
-  for (let t of infoTable) {
-    if (t.tHead) {
-      changeLinks(t);
+  for (let t of [...mainTable, ...infoTable]) {
+    if (t.tBodies) {
+      changeLinks(t.tBodies[0]);
     }
   }
 })();
